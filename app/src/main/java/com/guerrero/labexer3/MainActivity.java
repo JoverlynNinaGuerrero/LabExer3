bec9b1e63bad090c4b138e02ce6f2171ab143838
JoverlynNinaGuerrero <joverlynnina.guerrero.iics.@ust.edu.ph>
2017-11-09 23:58:27 +0800

    commit

--- app/src/main/java/com/guerrero/labexer3/MainActivity.java
+++ app/src/main/java/com/guerrero/labexer3/MainActivity.java
@@ -1,13 +1,122 @@
 package com.guerrero.labexer3;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    EditText et_Data;
+    EditText et_Filename;
+    FileInputStream fis;
+    TextView text_View;
+    TextView text_View2;
+    FileOutputStream fos;
+    Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_Data = (EditText) findViewById(R.id.etData);
+        text_View = (TextView) findViewById(R.id.textView);
+        text_View2 = (TextView) findViewById(R.id.textView2);
+        et_Filename = (EditText) findViewById(R.id.etFilename);
+        btn_next = (Button) findViewById(R.id.btn_Next);
+        preferences = getSharedPreferences(et_Filename.getText().toString(), Context.MODE_PRIVATE);
     }
+
+    public void writeData(File file, String message){
+
+        try{
+            fos = new FileOutputStream(file);
+            fos.write(message.getBytes());
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void nexttt (View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void sharedPref(View view){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("message",et_Data.getText().toString());
+        editor.commit();
+        Toast.makeText(this,"Successfully Written to Shared Preference",Toast.LENGTH_LONG).show();
+    }
+    public void intStore(View view) {
+        String message = et_Data.getText().toString();
+        try{
+            fos = openFileOutput(et_Filename.getText().toString(), Context.MODE_PRIVATE);
+            fos.write(message.getBytes());
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Successfully Written to Internal Storage", Toast.LENGTH_LONG).show();
+    }
+    public void intCache(View view){
+        File folder = getCacheDir();
+        File file = new File(folder,et_Filename.getText().toString());
+        String message = et_Data.getText().toString();
+        writeData(file,message);
+        Toast.makeText(this,"Successfully Written to Internal Cache",Toast.LENGTH_LONG).show();
+
+    }
+    public void extCache(View view){
+        File folder = getExternalCacheDir();
+        File file = new File(folder,et_Filename.getText().toString());
+        String message = et_Data.getText().toString();
+        writeData(file,message);
+        Toast.makeText(this,"Successfully Written to External Cache",Toast.LENGTH_LONG).show();
+    }
+    public void extStorage(View view){
+        File folder = getExternalFilesDir("Temp");
+        File file = new File(folder,et_Filename.getText().toString());
+        String message = et_Data.getText().toString();
+        writeData(file,message);
+        Toast.makeText(this,"Successfully Written to External Storage",Toast.LENGTH_LONG).show();
+    }
+    public void extPublic(View view){
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(folder,et_Filename.getText().toString());
+        String message = et_Data.getText().toString();
+        writeData(file,message);
+        Toast.makeText(this,"Successfully Written to External Public Storage",Toast.LENGTH_LONG).show();
+
+    }
+
 }
--- app/src/main/java/com/guerrero/labexer3/SecondActivity.java
+++ app/src/main/java/com/guerrero/labexer3/SecondActivity.java
@@ -1,13 +1,127 @@
 package com.guerrero.labexer3;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+    SharedPreferences preferences;
+    TextView tv_display;
+    EditText et_Filename;
+    FileInputStream fis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tv_display = (TextView) findViewById(R.id.tv_display);
+        et_Filename = (EditText) findViewById(R.id.etFilename);
+        preferences = getSharedPreferences(et_Filename.getText().toString(), Context.MODE_PRIVATE);
+    }
+    public void loadShared(View view){
+        String message = preferences.getString("message","");
+        tv_display.setText(message);
+
+    }
+    public void loadIntStore(View view){
+        StringBuffer buffer = new StringBuffer();
+        int read=0;
+        try{
+            fis = openFileInput(et_Filename.getText().toString());
+            while((read = fis.read()) != -1){
+                buffer.append((char)read);
+            }
+            fis.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        tv_display.setText(buffer.toString());
+
+    }
+    public void loadIntCache(View view){
+        StringBuffer stringBuffer = new StringBuffer();
+        File folder = getCacheDir();
+        File file = new File(folder,et_Filename.getText().toString());
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(file);
+            int read=1;
+
+            while((read = fis.read())!=-1){
+                stringBuffer.append((char)read);
+            }
+        }catch(Exception e){
+
+        }
+        tv_display.setText(stringBuffer.toString());
+    }
+    public void loadExtCache(View view){
+        StringBuffer stringBuffer = new StringBuffer();
+        File folder = getExternalCacheDir();
+        File file = new File(folder,et_Filename.getText().toString());
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(file);
+            int read=1;
+
+            while((read = fis.read())!=-1){
+                stringBuffer.append((char)read);
+            }
+        }catch(Exception e){
+
+        }
+        tv_display.setText(stringBuffer.toString());
+    }
+    public void loadExtStorage(View view){
+        StringBuffer stringBuffer = new StringBuffer();
+        File folder = getExternalFilesDir("Temp");
+        File file = new File(folder,et_Filename.getText().toString());
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(file);
+            int read=1;
+
+            while((read = fis.read())!=-1){
+                stringBuffer.append((char)read);
+            }
+        }catch(Exception e){
+
+        }
+        tv_display.setText(stringBuffer.toString());
+    }
+    public void loadExtPublic(View view){
+        StringBuffer stringBuffer = new StringBuffer();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(folder,et_Filename.getText().toString());
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(file);
+            int read=1;
+
+            while((read = fis.read())!=-1){
+                stringBuffer.append((char)read);
+            }
+        }catch(Exception e){
+
+        }
+        tv_display.setText(stringBuffer.toString());
+    }
+    public void prev(View view){
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
